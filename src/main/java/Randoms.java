080ebb1d1065cd5452216847f44d0ad44f2d8f24
Ekaterina Drobysheva <katerina_1_777@list.ru>
2023-03-05 21:49:55 +0300

    add Origin

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min, max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return new Random().nextInt((max + 1) - min) + min;
+            }
+        };
+    }
+}
